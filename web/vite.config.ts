33688811ec44bf1325aad691f2c4c001dc4fecbf
jrush-vj <vijayjerush4687@gmail.com>
2026-09-24 10:21:59 +0530

    tested on ngrok

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
   },
   server: {
     port: 5173,
+    allowedHosts: ["f055-182-74-196-22.ngrok-free.app"],
     proxy: {
       "/api": { target: "http://127.0.0.1:5000", changeOrigin: true },
     },
